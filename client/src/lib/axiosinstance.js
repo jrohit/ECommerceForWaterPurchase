3bed86cb31be3c8ec719cb85791173f824b120b2
Rohit J <4178929+jrohit@users.noreply.github.com>
2023-07-22 23:36:54 +0530

    changing axios instance

--- client/src/lib/axiosinstance.js
+++ client/src/lib/axiosinstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(process.env.NODE_ENV);
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Replace with your backend API URL
+  baseURL:
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:8000", // Replace with your backend API URL
   timeout: 5000, // Set a timeout value (in milliseconds) for requests
   headers: {
     "Content-Type": "application/json", // Set the default content type for requests
